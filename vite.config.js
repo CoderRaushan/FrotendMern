cef952024f1dfaf0b27d1ce802019dd8e7a5afbe
CoderRaushan <raushankumar23082004@gmail.com>
2024-11-11 10:55:15 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   // base: '/FrotendMern/', 
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/FrotendMern/', 
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // allows Render to access the server
+    port: 10000, // sets a default port
+  },
 })
